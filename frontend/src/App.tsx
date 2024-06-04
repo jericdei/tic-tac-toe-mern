1f9475ec67e15acc57b57ee6d2d5c070855efa0b
Jeric June Logan <loganjeric25@gmail.com>
2024-06-04 21:02:22 +0800

    setup frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
-import { useState } from "react";
+import { Button } from "primereact/button"
 
 function App() {
-    return <p>nice</p>;
+    return (
+        <>
+            <main className="grid h-screen w-screen place-items-center">
+                <div className="p-4">
+                    <h1 className="text-6xl font-bold">Tic-Tac-Toe</h1>
+
+                    <div className="mt-8">
+                        <pre className="text-center">list of previous games here</pre>
+                    </div>
+
+                    <div className="mt-8 flex justify-center">
+                        <Button label="New Game" />
+                    </div>
+                </div>
+            </main>
+        </>
+    )
 }
 
-export default App;
+export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import { PrimeReactProvider } from "primereact/api"
+import Tailwind from "primereact/passthrough/tailwind"
+import { twMerge } from "tailwind-merge"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <PrimeReactProvider
+        value={{
+            unstyled: true,
+            pt: Tailwind,
+            ptOptions: { mergeSections: true, mergeProps: true, classNameMergeFunction: twMerge },
+        }}
+    >
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </PrimeReactProvider>,
 )
